7f0bc812ce28889d2726526dec24b4ad346253e5
kkito0726 <k.kito0726@gmail.com>
2023-01-13 17:52:02 +0900

    複数人を処理して合否判定できるようにした。三項演算子等について学習

--- student.rb
+++ student.rb
@@ -13,17 +13,40 @@ class Student
     end
 
     def judge(avg, border)
-        if avg > border
-            puts "合格！！！"
-        else
-            puts "不合格"
-        end
+        avg > border ? true : false
     end
 
     attr_accessor :name
 end
 
-a001 = Student.new("kito")
-avg = a001.cal_avg([90,30,70])
-puts a001.name
-a001.judge(avg, 60)
+
+def main()
+    students = ["A", "B", "C", "D"]
+    students_num = students.length
+    scores = [
+        [68, 77, 98, 34, 56],
+        [58, 86, 73, 29, 47],
+        [78, 93, 85, 73, 85],
+        [29, 39, 48, 53, 29]
+    ]
+    border = 60
+    avg_list = Array.new(students_num)
+    isPassed = Array.new(students_num)
+    for i in 0..students_num-1 do
+        student = Student.new(students[i])
+        avg_list[i] = student.cal_avg(scores[i])
+        isPassed[i] = student.judge(avg_list[i], border)
+    end
+
+    # 合否結果を出力
+    print "氏名を入力: "
+    name = gets.chomp
+    id = students.index(name)
+    if isPassed[id]
+        puts "平均点は#{avg_list[id]}点で合格です！！"
+    else
+        puts "平均点は#{avg_list[id]}点で、あと#{border-avg_list[id]}点足りませんでした..."
+    end
+end
+
+main()
